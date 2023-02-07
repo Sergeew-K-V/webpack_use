c3abbd8c673c529b0020205c72059731546ff3d6
kirill.sergeev <kirill.sergeev@progforce.com>
2023-02-07 16:01:56 +0300

    Output management

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import "./style.css";
 import Icon from "./public/images/icon.jpeg";
 import dataXML from "./public/data/data.xml";
 import dataCSV from "./public/data/data.csv";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
   const myIcon = new Image();
   myIcon.src = Icon;
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   console.log(dataXML);
   console.log(dataCSV);
