58de3f09359d780764ef10cad237fe498eb51499
kirill.sergeev <kirill.sergeev@progforce.com>
2023-02-07 15:22:56 +0300

    Assets managmets finish

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import _ from "lodash";
 import "./style.css";
+import Icon from "./public/images/icon.jpeg";
+import dataXML from "./public/data/data.xml";
+import dataCSV from "./public/data/data.csv";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  console.log(dataXML);
+  console.log(dataCSV);
+  element.appendChild(myIcon);
   return element;
 }
 
