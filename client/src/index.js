fc421afa0d930bb517838ee7849d7cfdcb87c3d5
kirill.sergeev <kirill.sergeev@progforce.com>
2023-02-08 14:10:39 +0300

    server

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import printMe from "./print.js";
 function component() {
   const element = document.createElement("div");
   const btn = document.createElement("button");
-
+  const btnServer = document.createElement("button");
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
   const myIcon = new Image();
@@ -16,6 +16,16 @@ function component() {
 
   btn.innerHTML = "Click me and check the console!";
   btn.onclick = printMe;
+  btnServer.innerHTML = "Send to server!";
+  element.appendChild(btnServer);
+  btnServer.addEventListener("click", async () => {
+    try {
+      await fetch("http://127.0.0.1:8080/");
+      console.log("Success!");
+    } catch (error) {
+      console.log("Something was wrong:", error);
+    }
+  });
 
   element.appendChild(btn);
 
